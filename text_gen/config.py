2a8551bcf6e724373cc91749aea7d76ef2912a17
Abhinav sb <abhinavszb@gmail.com>
2024-09-20 01:57:43 +0530

    models.py completed

--- text_gen/config.py
+++ text_gen/config.py
@@ -1,4 +1,5 @@
 import torch
+from utils import #
 
 batch_size = 16
 block_size = 32
@@ -11,6 +12,7 @@ n_embd = 64
 n_head = 4
 n_layer = 4
 dropout = 0
+vocab_size =len(chars) #64 is test value this is the vocab size real value =len(chars) and chars should import from the utils
 
 
 torch.manual_seed(1337)
--- text_gen/models/bigram_language_model.py
+++ text_gen/models/bigram_language_model.py
@@ -1,14 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from text_gen.config import n_embd, block_size, head_size, n_layer, vocab_size, dropout, device
+from ..config import n_embd, n_head, block_size, n_layer, vocab_size, dropout, device
 
 class Head(nn.Module):
 
     def __init__(self, n_embd, head_size):
         super().__init__()
         self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)   
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size,block_size)))
         
@@ -77,7 +77,52 @@ class BigramLanguageModel(nn.Module):
 
     def __init__(self):
         super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size,n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd, vocab_size)#learn about layer norm
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx)
+        pos_emb = self.position_embedding_table(idx)
+        x = tok_emb + pos_emb # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
         
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
+    def generate(self, idx, max_new_toknes):
+        # idx is (B, T) array of indices in the current context
+        for _  in range(max_new_toknes):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)# (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)# (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)# (B, T+1)
+        return idx
+
+
+
+
     
 
 
